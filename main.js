1066b5008f2feea6c0fcbf7e833e0c86bae890b2
Fellipe Barnabe <fellipebarnabe.stack@gmail.com>
2023-09-20 20:28:23 -0300

    lastCommit

--- main.js
+++ main.js
@@ -5,16 +5,19 @@ const button = document.querySelector('#button');
 const selector = document.querySelector('#option');
 const textoDeSaida = document.querySelector('#answer');
 
+const UUID_VERSION = 4
+
 button.addEventListener('click', (event) => {
   // criamos um objeto cujas chaves são os tipos a
   // serem validados. Por exemplo, a chave CPF valida se
   // o campoDeTexto.value é um CPF
+  event.preventDefault();
 
   const campos = {
     cpf: validator.isTaxID(campoDeTexto.ariaValueMax, 'pt-BR'),
     hexColor: validator.isTaxID(campoDeTexto.value),
     email: validator.isEmail(campoDeTexto.value),
-    uuid: validator.isUUID(campoDeTexto, value, 4),
+    uuid: validator.isUUID(campoDeTexto, UUID_VERSION),
     url: validator.isURL(campoDeTexto.value),
   };
     // O objeto "campos" possui as chaves com o mesmo nome
@@ -22,3 +25,6 @@ button.addEventListener('click', (event) => {
     // selecionar a chave de acordo com o selecionado no HTML
   textoDeSaida.innerHTML = `A validação retornou ${campos[selector.value]}`;
 });
+
+
+
